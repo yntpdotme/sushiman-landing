31992a7d1f70b4d4f383494c52901927d3b007e6
Akash Kadlag <akashkadlag14@gmail.com>
2024-02-09 18:50:45 +0530

    feat: implement dropdown-menu for mobile devices

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ AOS.init({
 	duration: 1000,
 	offset: 100,
 });
+
+document.addEventListener("click", (e) => {
+	const isDropdownButton = e.target.matches("[data-dropdown-button]");
+	if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+
+	let currentDropdown;
+	if (isDropdownButton) {
+		currentDropdown = document.querySelector(".header__dropdown");
+		currentDropdown.classList.toggle("active");
+	}
+
+	document.querySelectorAll(".active").forEach((dropdown) => {
+		if (dropdown === currentDropdown) return;
+		dropdown.classList.remove("active");
+	});
+});
