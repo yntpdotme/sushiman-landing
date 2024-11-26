dda0a5c609a581e79972df0882e4fe1d40c66add
Akash Kadlag <akashkadlag14@gmail.com>
2024-11-26 18:00:40 +0530

    style: format using prettier

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ import "aos/dist/aos.css";
 
 // init AOS animation
 AOS.init({
-	duration: 1000,
-	offset: 100,
+  duration: 1000,
+  offset: 100,
 });
 
 document.addEventListener("click", (e) => {
-	const isDropdownButton = e.target.matches("[data-dropdown-button]");
-	if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
-	let currentDropdown;
-	if (isDropdownButton) {
-		currentDropdown = document.querySelector(".header__dropdown");
-		currentDropdown.classList.toggle("active");
-	}
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = document.querySelector(".header__dropdown");
+    currentDropdown.classList.toggle("active");
+  }
 
-	document.querySelectorAll(".active").forEach((dropdown) => {
-		if (dropdown === currentDropdown) return;
-		dropdown.classList.remove("active");
-	});
+  document.querySelectorAll(".active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
 });
